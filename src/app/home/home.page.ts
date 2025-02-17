cf13671f25ddf164946ac5e209172a265353b51e
Aluno <etecae023@hotmail.com>
2025-02-17 19:55:15 -0300

    appCalculadora

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,4 +10,55 @@ export class HomePage {
 
   constructor() {}
 
+
+  n1=0;
+  n2=0;
+  res=0;
+
+  somar(){
+    this.res=this.n1+this.n2;
+  }
+
+  subtrair(){
+    this.res=this.n1-this.n2;
+  }
+
+  multiplicar(){
+    this.res=this.n1*this.n2;
+  }
+
+  dividir(){
+
+    if(this.n2!=0){
+      this.res=this.n1/this.n2;
+    }
+
+    else{
+      this.res=0;
+}}
+
+  pes=0;
+  km=0;
+
+  converter() {
+    this.km=this.pes*0.0003048;
+  }
+
+  mensagem="";
+  idade=0;
+
+  clas(){
+    if(this.idade<=0){
+      this.mensagem="Valor inválido: insira uma idade acima de 0.";
+    } else if(this.idade<=12){
+      this.mensagem="Você é uma criança.";
+    }else if(this.idade<=17){
+      this.mensagem="Você é um adolescente.";
+    }else if(this.idade<=59){
+      this.mensagem="Você é um adulto.";
+    }else {
+      this.mensagem="Você é um idoso.";
+    }
+  }
 }
+
